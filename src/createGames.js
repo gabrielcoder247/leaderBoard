0071aa32aec4a2b10b7420235d83c98bb29e3ea4
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-10-21 11:43:45 +0100

    Add creatGames functions

---
+++ src/createGames.js
@@ -0,0 +1,24 @@
+let gameId = '';
+const baseUri = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+const createGame = async() => {
+    const response = await fetch(baseUri, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: 'foo',
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    response.json().then((json) => {
+        const [, , , d] = json.result.split(' ');
+        gameId = d;
+
+    });
+};
+
+
+
+
+export { createGame, gameId, baseUri };
