b063d42ead0ad1de28c0a9c1d82a356921d7522c
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-10-21 12:11:14 +0100

    Fix eslintrc error

--- src/addScore.js
+++ src/addScore.js
@@ -1,16 +1,16 @@
 import { baseUri, gameId } from './createGames.js';
 
-const addScore = async(name, score) => {
-    const response = await fetch(`${baseUri}${gameId}/scores`, {
-        method: 'POST',
-        body: JSON.stringify({
-            user: name,
-            score: score,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
-    return response.json();
+const addScore = async (name, score) => {
+  const response = await fetch(`${baseUri}${gameId}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return response.json();
 };
 export default addScore;
--- src/createGames.js
+++ src/createGames.js
@@ -1,24 +1,20 @@
-let gameId = '';
+let gameId = ''; // eslint-disable-line
 const baseUri = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
-const createGame = async() => {
-    const response = await fetch(baseUri, {
-        method: 'POST',
-        body: JSON.stringify({
-            name: 'foo',
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
-    response.json().then((json) => {
-        const [, , , d] = json.result.split(' ');
-        gameId = d;
-
-    });
+const createGame = async () => {
+  const response = await fetch(baseUri, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'foo',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  response.json().then((json) => {
+    const [, , , d] = json.result.split(' ');
+    gameId = d;
+  });
 };
-
-
-
 
 export { createGame, gameId, baseUri };
